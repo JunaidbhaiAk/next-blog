dea0b93180e364c5f88cfcff8940a19e0314988c
Junaid <junaidhonor8x@gmail.com>
2022-02-28 19:37:45 +0530

    apollo-fix

--- utils/prismicHelper.js
+++ utils/prismicHelper.js
@@ -1,4 +1,4 @@
-import { PrismicLink } from "apollo-link-prismic";
+var PrismicLink = require("apollo-link-prismic").PrismicLink;
 import { InMemoryCache } from "apollo-cache-inmemory";
 import ApolloClient from "apollo-client";
 
